d662c02c2d8cf7c2227b7c845a888c95517cf46a
picture <savemypicture1@gmail.com>
2024-02-25 22:16:07 +0200

    translation bot

---
+++ bot.py
@@ -0,0 +1,32 @@
+import telebot
+import os
+import dotenv
+from googletrans import Translator
+
+dotenv.load_dotenv()
+TOKEN = os.getenv('TG_TOKEN')
+bot = telebot.TeleBot(TOKEN)
+
+
+def translate_text(text):
+    translator = Translator()
+    translated_text = translator.translate(text, dest='en')
+
+    return translated_text.text
+
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.reply_to(message, 'LEARN THE ENGLISH! BLEAT!!')
+
+
+@bot.message_handler(func=lambda message: True)
+def translate_message(message):
+    user_text = message.text
+
+    translated_text = translate_text(user_text)
+    bot.reply_to(message, f'{translated_text}')
+
+
+if __name__ == "__main__":
+    bot.polling(none_stop=True)
